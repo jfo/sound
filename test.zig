b187617d170d0c0568ab67880e23fd03ba766159
Jeff Fowler <jeffowler@gmail.com>
2022-07-03 00:33:23 +0200

    can link

--- test.zig
+++ test.zig
@@ -1,7 +1,11 @@
 const c = @cImport({
-    @cInclude("soundio/soundio.h");
+    @cInclude("/Volumes/InternalNVME/jeff/code/sound/libsoundio/soundio/soundio.h");
 });
 
 pub fn main() !void {
-    c.soundio_outstream_begin_write;
+    const thing = c.soundio_outstream_begin_write;
+
+    @import("std").debug.print("{}", .{thing});
+    // !!!!
+    // fn([*c].cimport:1:11.struct_SoundIoOutStream, [*c][*c].cimport:1:11.struct_SoundIoChannelArea, [*c]c_int) callconv(.C) c_int@1042b5f70⏎
 }

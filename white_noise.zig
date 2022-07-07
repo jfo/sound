b374467b97806dce5707a375fb4080a7d7254e4b
Jeff Fowler <jeffowler@gmail.com>
2022-07-07 10:18:58 +0200

    a little closer

--- white_noise.zig
+++ white_noise.zig
@@ -21,7 +21,9 @@ fn write_callback(outstream: [*c]c.SoundIoOutStream, frame_count_min: c_int, fra
 
         var frame: c_int = 0;
         while (frame < frame_count) {
-            const sample: f32 = std.rand.Isaac64.random(rng).float(f32);
+            var sample: f32 = std.rand.Isaac64.random(rng).float(f32);
+            sample = std.math.clamp(sample, @as(f32, -0.2), @as(f32, 0.2));
+            // std.debug.print("{x}\n", .{sample});
             var channel: u32 = 0;
 
             while (channel < layout.channel_count) {

6dc580013a1e066e7931d0c5856116900d87108e
Jeff Fowler <jeffowler@gmail.com>
2022-07-26 14:58:46 +0200

    add

--- main.zig
+++ main.zig
@@ -1,21 +1,21 @@
 const std = @import("std");
 const soundio = @import("./soundio.zig");
 
-var scaling: f32 = 0.0125;
+var scaling: f32 = 0.008;
 
 fn sine(pitch: f32, offset: f32) f32 {
     var radians_per_second: f32 = (pitch * 2.0) * std.math.pi;
     return std.math.sin(offset * radians_per_second);
 }
 
-fn saw(pitch: f32, frame: c_int, seconds_per_frame: f32) f32 {
-    return sine(pitch, frame, seconds_per_frame) +
-        sine(2.0 * pitch, frame, seconds_per_frame) +
-        sine(3.0 * pitch, frame, seconds_per_frame) +
-        sine(4.0 * pitch, frame, seconds_per_frame) +
-        sine(5.0 * pitch, frame, seconds_per_frame) +
-        sine(6.0 * pitch, frame, seconds_per_frame) +
-        sine(7.0 * pitch, frame, seconds_per_frame);
+fn saw(pitch: f32, offset: f32) f32 {
+    return sine(pitch, offset) +
+        sine(2.0 * pitch, offset) +
+        sine(3.0 * pitch, offset) +
+        sine(4.0 * pitch, offset) +
+        sine(5.0 * pitch, offset) +
+        sine(6.0 * pitch, offset) +
+        sine(7.0 * pitch, offset);
 }
 
 var prng = std.rand.DefaultPrng.init(0);
@@ -25,8 +25,10 @@ fn white() f32 {
 }
 
 fn getSample(offset: f32) f32 {
-    const s = sine(440.0, offset) * scaling;
-    return (white() * scaling) + s;
+    const w = white() * scaling;
+    const base = sine(220.0, offset) * scaling;
+    const s = saw(440.0, offset) * scaling * 5;
+    return w + base + s;
 }
 
 pub fn main() !void {
